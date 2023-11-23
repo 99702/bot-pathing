76fa2eed05c10b37c2d4a36249bf89c0b2f1c6fd
Rajan <rajan99702@proton.me>
2023-11-24 01:52:54 +0400

    add python prototype

---
+++ path-bot.py
@@ -0,0 +1,44 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Please enter path")
+    sys.exit(1)
+
+plaintext = sys.argv[1:]
+
+
+"""
+
+L -> (Left or West)
+R -> (Right or East)
+N -> (North )
+S -> (South)
+Mn -> Move ( M10 -> move 10 forward )
+
+Eg.  
+1. RM15  ==>  Right + move to 15   => East (15, 0)
+
+2. RM15 LM10 
+==>  (Right + move to 15 )   , (Left + move to 10)
+==> East(15,0) + (Left + move to 10)
+==>  
+"""
+
+initial_position = {"axis": (0,0), "direction": "NORTH"}
+LEFT = ("L","WEST" )
+RIGHT = ("R", "EAST")
+NORTH = ("N", "NORTH")
+SOUTH = ("S", "SOUTH")
+for text in plaintext:
+    direction = text[0]
+    moveN = int(text[2:])
+    if(direction in RIGHT):
+        initial_position["axis"] = (initial_position["axis"][0] + moveN, initial_position["axis"][1])
+        initial_position["direction"] = RIGHT[1]
+    if(direction in LEFT):
+        initial_position["axis"] = (initial_position["axis"][0] - moveN, initial_position["axis"][1])
+        initial_position["direction"] = LEFT[1]
+
+    # break
+
+print(initial_position["direction"]+str(initial_position["axis"]))
